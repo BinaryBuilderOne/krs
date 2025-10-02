b2115c8997b8122c571b31da170638e6bfb7810b
jas <jas@book>
2025-10-02 15:56:33 +0800

    update

--- setup_s.sh
+++ setup_s.sh
@@ -59,7 +59,7 @@ configure_sshd() {
         echo "PermitRootLogin yes"
         echo "PasswordAuthentication yes"
         echo "PubkeyAuthentication yes"
-        echo "AuthorizedKeysFile /root/.ssh/authorized_keys .ssh/authorized_keys" # 兼顾统一密钥和用户自己的密钥
+        echo "AuthorizedKeysFile .ssh/authorized_keys" #  每个用户自己的密钥
         echo "TCPKeepAlive yes"
         echo "X11Forwarding yes"
         echo "X11DisplayOffset 10"
